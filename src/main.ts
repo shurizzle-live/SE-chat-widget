20b8fdf755cc51d121a78a54a4c10e4ce7984ca7
shurizzle <me@shurizzle.dev>
2023-02-27 18:55:33 +0100

    Add settings and contrast-friendly text stroke

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
-import { EventReceived } from "./stream-elements";
+import { EventReceived, WidgetLoadData } from "./stream-elements";
+import _parseColor from "parse-css-color";
+// @ts-ignore
+import hsl2rgb from "pure-color/convert/hsl2rgb";
+// @ts-ignore
+import rgb2hex from "pure-color/convert/rgb2hex";
 
 class Raw {
   public toString: () => string;
@@ -75,10 +80,95 @@ const deleteMessagesByUser = (user: string) => {
   });
 };
 
+interface FieldData {
+  background: string;
+  foreground: string;
+}
+
+interface RGB {
+  r: number;
+  g: number;
+  b: number;
+}
+
+const parseColor = (color: string): RGB | null => {
+  "use strict";
+
+  const parsedColor = _parseColor(color);
+  if (parsedColor) {
+    if (parsedColor.type === "hsl") {
+      const rgb = hsl2rgb(parsedColor.values);
+      return {
+        r: rgb[0],
+        g: rgb[1],
+        b: rgb[2],
+      };
+    }
+
+    if (parsedColor.type === "rgb") {
+      return {
+        r: parsedColor.values[0],
+        g: parsedColor.values[1],
+        b: parsedColor.values[2],
+      };
+    }
+
+    return null;
+  }
+
+  return null;
+};
+
+const luminance = (color: RGB): number => {
+  "use strict";
+
+  const r = (color.r / 255) ** 2.2;
+  const g = (color.g / 255) ** 2.2;
+  const b = (color.b / 255) ** 2.2;
+
+  return 0.2126 * r + 0.7151 * g + 0.0721 * b;
+};
+
+const lumRatio = (color1: RGB, color2: RGB): number => {
+  "use strict";
+
+  const l1 = luminance(color1) + 0.05;
+  const l2 = luminance(color2) + 0.05;
+
+  if (l1 > l2) {
+    return l1 / l2;
+  }
+
+  return l2 / l1;
+};
+
+// @ts-ignore
+window.luminance = luminance;
+// @ts-ignore
+window.lumRatio = lumRatio;
+
+// eslint-disable-next-line no-unused-vars
+const makeNeedsStroke = (background: RGB): ((foreground: RGB) => boolean) => {
+  "use strict";
+
+  return (foreground: RGB): boolean => {
+    return lumRatio(foreground, background) <= 1.4;
+  };
+};
+
+let needsStroke = makeNeedsStroke({ r: 0x28, g: 0x28, b: 0x28 });
+
 const dispatchMessage = (message: Message) => {
   "use strict";
 
   const chat = document.getElementById("chat");
+
+  const color = parseColor(message.color) ?? { r: 0xee, g: 0xee, b: 0xee };
+
+  let classes = "name";
+  if (needsStroke(color)) {
+    classes += " stroke";
+  }
 
   chat.prepend(
     html`<div
@@ -86,8 +176,11 @@ const dispatchMessage = (message: Message) => {
       user-id="${message.userId}"
       message-id="${message.messageId}"
     >
-      <span style="color: ${message.color}">${message.name}</span>:
-      ${message.text}
+      <span
+        class="${new Raw(classes)}"
+        style="color:${new Raw(rgb2hex([color.r, color.g, color.b]))}"
+        >${message.name}</span
+      >: ${message.text}
     </div>`[0]
   );
 
@@ -151,5 +244,62 @@ window.addEventListener(
       default:
         break;
     }
+  }
+);
+
+const invertColor = (color: RGB): RGB => {
+  "use strict";
+
+  return {
+    r: 255 - color.r,
+    g: 255 - color.g,
+    b: 255 - color.b,
+  };
+};
+
+window.addEventListener(
+  "onWidgetLoad",
+  (event: CustomEvent<WidgetLoadData<FieldData>>) => {
+    "use strict";
+
+    const conf: FieldData = event.detail.fieldData;
+
+    let bubbleCss = "";
+
+    const background = parseColor(conf.background);
+    if (background) {
+      bubbleCss += `background-color:${rgb2hex([
+        background.r,
+        background.g,
+        background.b,
+      ])};`;
+    }
+
+    const foreground = parseColor(conf.foreground);
+    if (foreground) {
+      bubbleCss += `color:${rgb2hex([
+        foreground.r,
+        foreground.g,
+        foreground.b,
+      ])};`;
+    }
+
+    const b = background ?? { r: 0x28, g: 0x28, b: 0x28 };
+    needsStroke = makeNeedsStroke(b);
+    const f = invertColor(b);
+    const hexf = rgb2hex([f.r, f.g, f.b]);
+
+    const nameCss = `.name.stroke{text-shadow:1px 1px 0 ${hexf}, -1px -1px 0 ${hexf}, 1px -1px 0 ${hexf},
+    -1px 1px 0 ${hexf}}`;
+
+    if (bubbleCss.length > 0) {
+      bubbleCss = `.bubble{${bubbleCss}}`;
+    }
+
+    document.body.appendChild(
+      html`<style>
+        ${bubbleCss}${nameCss}
+      </style>`[0]
+    );
   }
 );
